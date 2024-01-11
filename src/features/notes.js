a6afa7d5572422fc56276c9c17906685e7c9d75c
Senrine <saadinesrine16@gmail.com>
2024-01-11 21:39:21 +0100

    store added

---
+++ src/features/notes.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { data } from "autoprefixer";
+import { useEffect } from "react";
+
+const initialState = {
+  list: undefined,
+};
+
+const notes = createSlice({
+  name: "notes",
+  initialState,
+  reducers: {
+    addNotesFromApi: (state, action) => {
+      state.list = action.payload;
+    },
+  },
+});
+
+export function getNotesFromApi(action) {
+  return function (dispatch, getState) {
+    fetch("/data/notes.json")
+      .then((response) => response.json())
+      .then((data) => dispatch(addNotesFromApi(data.notes)));
+  };
+}
+
+export const { addNotesFromApi } = notes.actions;
+export default notes.reducer;
---
+++ src/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import notes from "./features/notes";
+export const store = configureStore({
+  reducer: {
+    notes,
+  },
+});
